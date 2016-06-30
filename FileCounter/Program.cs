94cbb66e557dadbe5211d28c92b496240dd7f048
Eric Anderson <elanderson@gmail.com>
2016-06-29 20:49:22 -0500

    Add files via upload

---
+++ FileCounter/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var fileCount = Directory.EnumerateFiles(Directory.GetCurrentDirectory(), 
+                                                     "*.*", 
+                                                     SearchOption.AllDirectories).Count();
+            Console.WriteLine($"{Directory.GetCurrentDirectory()} continas {fileCount} files");
+        }
+    }
+}
